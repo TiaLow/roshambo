cfbce64493cbeb61e1c76b49ebbbb93e67b5c647
Tia Low <lowtia@gmail.com>
2021-01-05 12:49:18 -0800

    Installed express and dotenv, basic express server setup, broke out PORT to .env file, started server to see console log as proof of server life

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+// const router = require('./router.js');
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+});
+
+
+
