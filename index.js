0b0132f3dd5bb115cc9e294583004a066d87edfd
Tia Low <lowtia@gmail.com>
2021-01-05 13:06:59 -0800

    Required error handler files into index and using them after router, manually tested 404 in browser

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const router = require('./router.js');
+const errorHandler404 = require('./middleware/404.js');
+const errorHandler500 = require('./middleware/500.js');
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
 app.use(router);
+app.use('*', errorHandler404);
+app.use(errorHandler500);
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
