ef010ef5bdf183865731c43640168b50ae04280f
Tia Low <lowtia@gmail.com>
2021-01-05 13:28:35 -0800

    Broke out server work to separate file so that index can just start the server so that I can also use app in testing. Re testing, I installed supertest to test endpoints

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 'use strict';
 
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const router = require('./router.js');
-const errorHandler404 = require('./middleware/404.js');
-const errorHandler500 = require('./middleware/500.js');
+
+const server = require('./server.js');
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use(router);
-app.use('*', errorHandler404);
-app.use(errorHandler500);
-
-app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`);
-});
+server.start(PORT);
 
 
 
