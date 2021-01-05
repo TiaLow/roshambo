dcf62f3c06bca366ad147bdcb74f01c797f03b49
Tia Low <lowtia@gmail.com>
2021-01-05 12:57:11 -0800

    Hello World proof of life with GET on / route, broke it out to a router file

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const router = require('./router.js');
+const router = require('./router.js');
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 
+app.use(router);
+
 app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`)
+  console.log(`listening on ${PORT}`);
 });
 
 
