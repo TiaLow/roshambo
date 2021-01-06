07f65d6d6c09058896e7c3554599acae1693e130
Tia Low <lowtia@gmail.com>
2021-01-05 18:13:15 -0800

    Saving players name in global variable, tracking player and app scores in global variables, building leaderboard arr of objs with that information

--- router.js
+++ router.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 // ------ GLOBAL VARS
 
+let currentPlayer;
+let userScore = 0;
+let appScore = 0;
+
 
 // ------ ROUTES
 
@@ -28,6 +32,8 @@ function handleTurn(req, res) {
   // deconstructing the req.query obj to grab the player name and play from the query params
   const { player_name, play } = req.query;
 
+  currentPlayer = player_name;
+
   // randomizing the app's play
   let appPlay = roshambo[Math.floor(Math.random() * roshambo.length)];
 
@@ -37,8 +43,10 @@ function handleTurn(req, res) {
     if (appPlay === 'rock'){
       res.status(200).send(`${player_name} ties this round`);
     } else if (appPlay === 'paper'){
+      appScore++;
       res.status(200).send(`${player_name} loses this round`);
     } else {
+      userScore++;
       res.status(200).send(`${player_name} wins this round`);
     }
     break;
@@ -47,8 +55,10 @@ function handleTurn(req, res) {
     if (appPlay === 'paper'){
       res.status(200).send(`${player_name} ties this round`);
     } else if (appPlay === 'scissors'){
+      appScore++;
       res.status(200).send(`${player_name} loses this round`);
     } else {
+      userScore++;
       res.status(200).send(`${player_name} wins this round`);
     }
     break;
@@ -57,8 +67,10 @@ function handleTurn(req, res) {
     if (appPlay === 'scissors'){
       res.status(200).send(`${player_name} ties this round`);
     } else if (appPlay === 'rock'){
+      appScore++;
       res.status(200).send(`${player_name} loses this round`);
     } else {
+      userScore++;
       res.status(200).send(`${player_name} wins this round`);
     }
     break;
@@ -67,10 +79,22 @@ function handleTurn(req, res) {
     res.status(404).send('Unknown play, please ro-sham-bo again.');
   }
 
-  
+  console.log('user score:   ', userScore);
+  console.log('app score:  ', appScore);
+
 }
 
 function handleLeaderboard(req, res) {
+
+  let leaderboard = [{
+    name: currentPlayer,
+    score: userScore,
+  }, {
+    name: 'Computer',
+    score: appScore,
+  }];
+
+  console.log('leaderboard:   ', leaderboard);
   
   res.status(200).send('This will be the sorted list of high scores.');
 
