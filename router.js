4111b7a6f0cc8d9a41111ef102727e59c8e0f736
Tia Low <lowtia@gmail.com>
2021-01-05 17:14:28 -0800

    Broke route handlers out to separate functions for organization

--- router.js
+++ router.js
@@ -3,8 +3,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(request, response){
-  response.send('Hello New World');
-});
+// ------ ROUTES
+
+router.get('/', renderHome);
+router.get('/shoot', renderTurn);
+router.get('/leaderboard', renderLeaderboard);
+
+
+// ------ ROUTE HANDLERS
+
+function renderHome(req, res) {
+  
+  res.status(200).send('Let\'s ROSHAMBO!');
+  
+}
+
+function renderTurn(req, res) {
+  
+  res.status(200).send('This player wins/loses/ties this round.');
+  
+}
+
+function renderLeaderboard(req, res) {
+  
+  res.status(200).send('This will be the sorted list of high scores.');
+
+}
+
+
+
 
 module.exports = router;
