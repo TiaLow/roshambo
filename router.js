c4cdc4a6030d304dd49e49b10b324a3f0024a5bd
Tia Low <lowtia@gmail.com>
2021-01-05 17:26:30 -0800

    Grabbing /shoot query params from hard typing into url from browser, deconstructing obj to get player name and play, rendering player name to page

--- router.js
+++ router.js
@@ -3,11 +3,14 @@
 const express = require('express');
 const router = express.Router();
 
+// ------ GLOBAL VARS
+
+
 // ------ ROUTES
 
 router.get('/', renderHome);
-router.get('/shoot', renderTurn);
-router.get('/leaderboard', renderLeaderboard);
+router.get('/shoot', handleTurn);
+router.get('/leaderboard', handleLeaderboard);
 
 
 // ------ ROUTE HANDLERS
@@ -18,13 +21,18 @@ function renderHome(req, res) {
   
 }
 
-function renderTurn(req, res) {
+function handleTurn(req, res) {
+
+  console.log('query params:  ', req.query);
+
+  // deconstructing the req.query obj to grab the player name and play from the query params
+  const { player_name, play } = req.query;
   
-  res.status(200).send('This player wins/loses/ties this round.');
+  res.status(200).send(`${player_name} wins/loses/ties this round.`);
   
 }
 
-function renderLeaderboard(req, res) {
+function handleLeaderboard(req, res) {
   
   res.status(200).send('This will be the sorted list of high scores.');
 
